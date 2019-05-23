2c3bd0471eb68714b4eaa5d79959def2c3dd971f
Jose luiz <37257235+zekkametallica@users.noreply.github.com>
2019-05-23 20:38:55 -0300

    Add files via upload
    for deface

---
+++ chat/dev.php
@@ -0,0 +1,35 @@
+<html>
+ <head>
+ <title>Bem vindo ao meu chat</title>
+ </head>
+ <body>
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+<!-- hehehehe-->
+<h2>Hey seu site foi hackeado, mas nao fique Triste, vamos adorar o sol </h2>
+
+
+<img src="https://gifimage.net/wp-content/uploads/2017/06/praise-the-sun-gif.gif" width="320" height="320" alt="Praise">
+
+<h1> Eu to Pobreeee </h1>
+
+<form  action="/hacked" method="post">
+ vc poderia botar a senha do seu cartao de credito aqui?<input type="text" name="cartao" value="0" />
+ <input type="submit" value="te pago 10 conto amanha , confia" />
+</form>
+
+<script>
+	var audio = new Audio('tranquilo.mp3');
+	audio.play()
+</script>
+
+
+<b> Seu site foi ownado pelo 3X0D14 , obrigado pela atencao! </b>
+
+<!-- hehehehe-->
+ </body>
+</html>
